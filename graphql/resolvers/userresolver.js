1423f6ef691a3328d5c0837d064cf641932c70d8
Kyle Phillips <khillips375@yahoo.com>
2020-02-09 00:45:23 -0500

    get users query

--- graphql/resolvers/userresolver.js
+++ graphql/resolvers/userresolver.js
@@ -1,10 +1,19 @@
-const usermodel =require('../../models/blogmodel');
+const Usermodel = require('../../models/blogmodel');
 
 const userResolver = {
     RootQuery: {
-
+        getUsers: async () => {
+            try {
+                const users = await Usermodel.findAll()
+                return users
+            } catch (err) {
+                throw err;
+            }
+        }
     },
     RootMutation: {
-        
+
     }
 }
+
+module.exports = userResolver
