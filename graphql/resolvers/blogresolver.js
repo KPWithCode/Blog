112e95a5b559d05e7a1713974a8052d254dbddce
Kyle Phillips <khillips375@yahoo.com>
2020-02-09 00:44:19 -0500

    blog resolver

--- graphql/resolvers/blogresolver.js
+++ graphql/resolvers/blogresolver.js
@@ -1,10 +1,43 @@
-const blogmodel =require('../../models/blogmodel');
+const Blogmodel = require('../../models/blogmodel');
 
 const blogResolver = {
     RootQuery: {
-
+        getBlogs: async () => {
+            try {
+                const blogs = await Blogmodel.findAll()
+                return blogs;
+            } catch (err) {
+                throw err
+            }
+        },
+        singleBlog: async (root, { id }) => {
+            try {
+                const oneBlog = await Blogmodel.findOne({
+                    where: {
+                        id: id
+                    }
+                });
+                return oneBlog
+            } catch (err) {
+                throw err
+            }
+        }
     },
     RootMutation: {
-        
+        createBlog: async (root, { title, content, contact, published }) => {
+            try {
+                const blog = await Blogmodel.create({
+                    title,
+                    content,
+                    contact,
+                    published
+                });
+                return blog;
+            } catch (err) {
+                throw err;
+            }
+        }
     }
 }
+
+module.exports = blogResolver
